7c48aa8081ecac843219ca2263b7a2529b410aa7
suzushin54 <mkm.snicr@gmail.com>
2024-12-13 23:42:04 +0900

    chore: sample app

---
+++ app/go.mod
@@ -0,0 +1,3 @@
+module github.com/suzushin54/multiple-containers/app
+
+go 1.23.4
---
+++ app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from Go!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Starting server on :8000")
+	http.ListenAndServe(":8000", nil)
+}
